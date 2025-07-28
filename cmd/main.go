098691ed57a9d394aa0fe594b6c72ea274d4dd07
TusharDayal1711 <tushar.dayal@remotestate>
2025-07-28 18:07:33 +0530

    add logger in main, just for test purpose

--- cmd/main.go
+++ cmd/main.go
@@ -11,8 +11,10 @@ func main() {
 
 	srv := server.ServerInit()
 	go srv.Start()
+	srv.Logger.GetLogger().Info("server initialized...")
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	<-done
 	srv.Stop()
+	srv.Logger.GetLogger().Info("server stopped...")
 }
