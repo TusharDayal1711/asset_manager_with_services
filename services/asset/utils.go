0f3fbec788c1cac8cb1e1656f0b07ea6f08749aa
TusharDayal1711 <tushar.dayal@remotestate>
2025-07-28 17:10:12 +0530

    wip

---
+++ services/asset/utils.go
@@ -0,0 +1,47 @@
+package assetservice
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"strings"
+	"time"
+)
+
+func IsAssetTypeValid(assetType string) bool {
+	return assetType == "laptop" || assetType == "mouse" || assetType == "monitor" || assetType == "hard_disk" ||
+		assetType == "pen_drive" ||
+		assetType == "mobile" ||
+		assetType == "sim" ||
+		assetType == "accessory"
+}
+
+func IsOwnershipValid(ownership string) bool {
+	return ownership == "remotestate" || ownership == "client"
+}
+
+func AssetValidityCheck(reqModel AddAssetWithConfigReq) error {
+	if strings.TrimSpace(reqModel.Brand) == "" {
+		return errors.New("brand is required")
+	}
+
+	if strings.TrimSpace(reqModel.Model) == "" {
+		return errors.New("model is required")
+	}
+
+	if strings.TrimSpace(reqModel.SerialNo) == "" {
+		return errors.New("serial number is required")
+	}
+
+	if reqModel.PurchaseDate.After(time.Now()) {
+		return errors.New("purchase date cannot be in the future")
+	}
+
+	if !IsAssetTypeValid(reqModel.Type) {
+		return fmt.Errorf("invalid asset type")
+	}
+
+	if !IsOwnershipValid(reqModel.OwnedBy) {
+		return fmt.Errorf("invalid owner ship")
+	}
+	return nil
+}
